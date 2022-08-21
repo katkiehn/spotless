382c617ce7587c9a3301eedf5ae2687d228d84b2
Katharina <katha.kiehn@gmail.com>
2022-08-21 10:58:14 +0200

    project setup

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Spotless
+
+Final project for Spiced Bootcamp
+A website to make your cleaning process easier
+
+## Set Up
+
+@ to do 
+description of what needs to be installed and how to run the project
+
+-install postgres
+-`npm install`
+-
---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const compression = require("compression");
+const path = require("path");
+const server = require("http").Server(app);
+
+app.use(compression());
+app.use(express.json());
+app.use(
+    express.static(
+        path.join(__dirname, "..", "..", "build", "client", "public")
+    )
+);
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+server.listen(process.env.PORT || 3001, function () {
+    console.log("I'm listening.");
+});
